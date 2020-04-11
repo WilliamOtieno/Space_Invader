49e0d9cefb551f0b9dc5c5a67cfc315cd96c8099
WilliamOtieno <jimmywilliamotieno@gmail.com>
2020-04-11 14:17:40 +0300

    Pushed changes

--- main.py
+++ main.py
@@ -6,8 +6,18 @@ pygame.init()
 #Create the screen
 screen = pygame.display.set_mode((800, 600))
 
+#Title and Icon
+pygame.display.set_caption("Space Invader")
+icon = pygame.image.load("spaceship.png")
+pygame.display.set_icon(icon)
+
+#Game loop
 running = True
 while running:
     for event in pygame.event.get():
-        if event.type == pygame.QUIT():
+        if event.type == pygame.QUIT:
             running = False
+    #RGB color red    
+    screen.fill((0, 0, 0))
+    pygame.display.update()
+
