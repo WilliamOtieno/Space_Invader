cb866d024255507eb1405400fc70baf8b9201531
WilliamOtieno <jimmywilliamotieno@gmail.com>
2020-04-11 19:07:18 +0300

    Added direction mechanisms

--- main.py
+++ main.py
@@ -11,13 +11,40 @@ pygame.display.set_caption("Space Invader")
 icon = pygame.image.load("spaceship.png")
 pygame.display.set_icon(icon)
 
+#Player
+playerImg = pygame.image.load('spaceship2.png')
+playerX = 370
+playerY = 480
+
+playerX_change = 0
+
+def player(x , y):
+    screen.blit(playerImg, (x, y))
+
 #Game loop
 running = True
 while running:
+
+    #RGB color red    
+    screen.fill((0, 0, 0))
+
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-    #RGB color red    
-    screen.fill((0, 0, 0))
+
+    #if keystroke is pressed check whethere it is LEFT or RIGHT
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                playerX_change = -0.3
+            if event.key == pygame.K_RIGHT:
+                playerX_change = 0.3
+
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                playerX_change = 0
+    
+    playerX += playerX_change
+    player(playerX, playerY)
     pygame.display.update()
 
